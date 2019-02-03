6aa64a9f59fce8b7705b18ca71fe663accb96bd6
nirmal1067 <nirmal1067@gmail.com>
2019-02-03 09:35:26 -0800

    Initial commit for elastic search

---
+++ src/main/java/org/myexample/elasticsearch/controller/BookController.java
@@ -0,0 +1,38 @@
+package org.myexample.elasticsearch.controller;
+
+import org.myexample.elasticsearch.bean.Book;
+import org.myexample.elasticsearch.dao.BookDao;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    private BookDao bookDao;
+
+    public BookController(BookDao bookDao) {
+        this.bookDao = bookDao;
+    }
+
+    @PostMapping
+    public Book insertBook(@RequestBody Book book) throws Exception{
+        return bookDao.insertBook(book);
+    }
+
+    @GetMapping("/{id}")
+    public Map<String, Object> getBookById(@PathVariable String id){
+        return bookDao.getBookById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Map<String, Object> updateBookById(@RequestBody Book book, @PathVariable String id){
+        return bookDao.updateBookById(id, book);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBookById(@PathVariable String id){
+         bookDao.deleteBookById(id);
+    }
+}
